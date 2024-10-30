d8988bbc36753979b5e0bcce04ad5f05da415c1f
CodeCanna <codecannamw@gmail.com>
2024-10-29 23:09:06 -0600

    Implemented new method for NearEarhObject.

--- src/lib.rs
+++ src/lib.rs
@@ -18,4 +18,30 @@ mod json_objects {
         miss_distance: f64,
         orbiting_body: String
     }
+
+    impl NearEarthObject {
+        pub fn new(
+            asteroid_id: String,
+            name: String,
+            estimagted_diameter: (f32, f32), // (min, max)
+            is_potentially_hazardous_asteroid: bool,
+            close_approach_date: time::Date,
+            close_approach_time: time::Time,
+            relative_velocity: f32,
+            miss_distance: f64,
+            orbiting_body: String
+        ) -> Self {
+            Self {
+                asteroid_id,
+                name,
+                estimagted_diameter,
+                is_potentially_hazardous_asteroid,
+                close_approach_date,
+                close_approach_time,
+                relative_velocity,
+                miss_distance,
+                orbiting_body
+            }
+        }
+    }
 }
