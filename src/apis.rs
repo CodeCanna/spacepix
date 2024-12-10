2afe299bf368618f983c71d9986ef57239ea33d2
CodeCanna <codecannamw@gmail.com>
2024-12-09 20:53:53 -0700

    Refactoring apis.rs

--- src/apis.rs
+++ src/apis.rs
@@ -71,7 +71,7 @@ impl Apod {
             Ok(r) => match json::parse(r.text().unwrap().as_str()) {
                 Ok(json_obj) => {
                     Ok(Self {
-                        copyright: json_obj["copyright"].to_string(),
+                       copyright: json_obj["copyright"].to_string(),
                        date: json_obj["date"].to_string(),
                        explanation: json_obj["explanation"].to_string(),
                        hdurl: json_obj["hdurl"].to_string(),
@@ -87,7 +87,7 @@ impl Apod {
         }
     }
 }
-
+#[derive(Debug, Clone, serde::Deserialize)]
 pub struct Links {
     next: String,
     previous: String,
@@ -104,66 +104,61 @@ impl Links {
     }
 }
 
-#[derive(Debug, Clone, serde::Deserialize, serde::Serialize)]
-pub struct NearEarthObject {
-    pub asteroid_id: String,
-    pub name: String,
-    pub estimated_diameter: (String, String), // (min, max)
-    pub is_potentially_hazardous_asteroid: bool,
-    pub close_approach_date: String,
-    pub close_approach_time: String,
-    pub relative_velocity: String,
-    pub miss_distance: String,
-    pub orbiting_body: String,
-}
-
-impl Default for NearEarthObject {
-    fn default() -> Self {
-        Self {
-            asteroid_id: String::default(),
-            name: String::default(),
-            estimated_diameter: (String::default(), String::default()),
-            is_potentially_hazardous_asteroid: bool::default(),
-            close_approach_date: String::default(),
-            close_approach_time: String::default(),
-            relative_velocity: String::default(),
-            miss_distance: String::default(),
-            orbiting_body: String::default()
-        }
-    }
-}
-
-impl NearEarthObject {
-    pub fn new(
-        asteroid_id: String,
-        name: String,
-        estimated_diameter: (String, String), // (min, max)
-    is_potentially_hazardous_asteroid: bool,
-    close_approach_date: String,
-    close_approach_time: String,
-    relative_velocity: String,
-    miss_distance: String,
-    orbiting_body: String,
-    ) -> Self {
-        Self {
-            asteroid_id,
-            name,
-            estimated_diameter,
-            is_potentially_hazardous_asteroid,
-            close_approach_date,
-            close_approach_time,
-            relative_velocity,
-            miss_distance,
-            orbiting_body,
-        }
-    }
-
-    // Get near earth object feed by date range
-    pub fn get_neows_feed(&self, start_date: &str, end_date: &str) {}
-
-    // Get unique near earth object by it's id
-    pub fn get_neows_by_id(&self, id: &str){}
-}
+// impl Default for NearEarthObject {
+//     fn default() -> Self {
+//         Self {
+//             asteroid_id: String::default(),
+//             name: String::default(),
+//             estimated_diameter: (String::default(), String::default()),
+//             is_potentially_hazardous_asteroid: bool::default(),
+//             close_approach_date: String::default(),
+//             close_approach_time: String::default(),
+//             relative_velocity: String::default(),
+//             miss_distance: String::default(),
+//             orbiting_body: String::default()
+//         }
+//     }
+// }
+//
+// impl NearEarthObject {
+//     pub fn new(
+//         asteroid_id: String,
+//         name: String,
+//         estimated_diameter: (String, String), // (min, max)
+//         is_potentially_hazardous_asteroid: bool,
+//         close_approach_date: String,
+//         close_approach_time: String,
+//         relative_velocity: String,
+//         miss_distance: String,
+//         orbiting_body: String,
+//     ) -> Self {
+//         Self {
+//             asteroid_id,
+//             name,
+//             estimated_diameter,
+//             is_potentially_hazardous_asteroid,
+//             close_approach_date,
+//             close_approach_time,
+//             relative_velocity,
+//             miss_distance,
+//             orbiting_body,
+//         }
+//     }
+//
+//     // Get near earth object feed by date range
+//     pub fn get_neows_feed(&self, date: str) Self {
+//         match reqwest::blocking::get(Parser::default()::neows_url()) {
+//             Ok(d) => {
+//                 Self {
+//
+//                 }
+//             }
+//         }
+//     }
+//
+//     // Get unique near earth object by it's id
+//     pub fn get_neows_by_id(&self, id: &str) {}
+// }
 
 // pub struct NearEarthObjectFeed {
 //     links: Links,
@@ -180,6 +175,33 @@ impl NearEarthObject {
 //         }
 //     }
 // }
+
+/**
+ * Representative of a NearEarthObject from the NASA API
+ * estimated_diameter tuple key ((feet_min, feet_max), (meters_min, meters_max))
+ * relative_velocity tuple key (kilometers_per_second, kilometers_per_hour, miles_per_hour)
+ * miss_distance tuple key (astronomical, lunar, kilometers, miles)
+ */
+pub struct NearEarthObject {
+    id: String,
+    neo_reference_id: String,
+    name: String,
+    estimated_diameter: ((u8, u8), (u8, u8)), // ((feet_min, feet_max), (meters_min, meters_max))
+    close_approach_date: String,
+    close_approach_date_full: String,
+    epoch_date_close_approach: u64,
+    relative_velocity: (String, String, String), // (kilometers_per_second, kilometers_per_hour, miles_per_hour)
+    miss_distance: (String, String, String, String), // (astronomical, lunar, kilometers, miles)
+    orbiting_body: String,
+    is_sentry_object: bool
+}
+
+#[derive(Debug, Clone, serde::Deserialize, serde::Serialize)]
+pub struct NEOFeed {
+    links: Links,
+    element_count: u8,
+    near_earth_objects: Vec<near_earth_object>
+}
 
 #[allow(dead_code)]
 struct DONKI {}
