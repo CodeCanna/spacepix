fa35ae277c387cac1454db00e716b4446d268871
CodeCanna <codecannamw@gmail.com>
2025-01-26 21:54:26 -0700

    Use serde_json to parse json from string.

--- src/main.rs
+++ src/main.rs
@@ -34,8 +34,9 @@ fn main() -> eframe::Result {
         Ok(mut f) => {
             let mut key = String::default();
             f.read_to_string(&mut key).unwrap();
-            let key_json = json::from(key);
+            let key_json: serde_json::Value = serde_json::from_str(&key.as_str()).unwrap();//json::from(key);
             let parser = Parser::new(key_json["key"].to_string());
+            // println!("{}", &parser.get_api_key());
             Ok(eframe::run_native(
             "Space Pix",
             native_options,
